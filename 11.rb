18ba1427032d843dd441b009bc0eb12eb600a82f
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-12-11 16:06:46 +0100

    day 11 part 2 OK

--- 11.rb
+++ 11.rb
@@ -1,65 +1,11 @@
 # # # PART ONE # # #
 
-giant_image = [
-  [".", ".", ".", "#", ".", ".", ".", ".", ".", "."],
-  [".", ".", ".", ".", ".", ".", ".", "#", ".", "."],
-  ["#", ".", ".", ".", ".", ".", ".", ".", ".", "."],
-  [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
-  [".", ".", ".", ".", ".", ".", "#", ".", ".", "."],
-  [".", "#", ".", ".", ".", ".", ".", ".", ".", "."],
-  [".", ".", ".", ".", ".", ".", ".", ".", ".", "#"],
-  [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
-  [".", ".", ".", ".", ".", ".", ".", "#", ".", "."],
-  ["#", ".", ".", ".", "#", ".", ".", ".", ".", "."]
-]
-
-after_expansion = [
-  [".", ".", ".", ".", "#", ".", ".", ".", ".", ".", ".", ".", "."],
-  [".", ".", ".", ".", ".", ".", ".", ".", ".", "#", ".", ".", "."],
-  ["#", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
-  [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
-  [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
-  [".", ".", ".", ".", ".", ".", ".", ".", "#", ".", ".", ".", "."],
-  [".", "#", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
-  [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "#"],
-  [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
-  [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
-  [".", ".", ".", ".", ".", ".", ".", ".", ".", "#", ".", ".", "."],
-  ["#", ".", ".", ".", ".", "#", ".", ".", ".", ".", ".", ".", "."]
-]
-
-universe_with_numbers = [
-  [".", ".", ".", ".", "1", ".", ".", ".", ".", ".", ".", ".", "."],
-  [".", ".", ".", ".", ".", ".", ".", ".", ".", "2", ".", ".", "."],
-  ["3", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
-  [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
-  [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
-  [".", ".", ".", ".", ".", ".", ".", ".", "4", ".", ".", ".", "."],
-  [".", "5", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
-  [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "6"],
-  [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
-  [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
-  [".", ".", ".", ".", ".", ".", ".", ".", ".", "7", ".", ".", "."],
-  ["8", ".", ".", ".", ".", "9", ".", ".", ".", ".", ".", ".", "."]
-]
-
-galaxies = {
-  1 => [4, 0],
-  2 => [9, 1],
-  3 => [0, 2],
-  4 => [8, 5],
-  5 => [1, 6],
-  6 => [12, 7],
-  7 => [9, 10],
-  8 => [0, 11],
-  9 => [5, 11]
-}
-# méthode pour doubler les lignes vides
-def double_empty_lines(arr)
+# méthode pour multiplier les lignes vides
+def double_empty_lines(arr, factor)
   double = []
   arr.each do |line|
     if line.count('.') == line.length
-      2.times { double << line }
+      factor.times { double << line }
     else
       double << line
     end
@@ -68,9 +14,9 @@ def double_empty_lines(arr)
 end
 
 # méthode pour expandre l'univers
-def expand_universe(arr)
-  vertical_expansion = double_empty_lines(arr.transpose)
-  double_empty_lines(vertical_expansion.transpose)
+def expand_universe(arr, factor)
+  vertical_expansion = double_empty_lines(arr.transpose, factor)
+  double_empty_lines(vertical_expansion.transpose, factor)
 end
 
 # méthode pour faire la liste des galaxies avec leurs coordonnées
@@ -109,6 +55,49 @@ def answer1(galaxies)
 end
 
 # # # PART TWO # # #
+
+# méthode pour trouver les indexes de chaque ligne ou colonne vide
+# entrée => giant_image
+# sortie => { 'empty_rows' => [3, 7], 'empty_columns' => [2, 5, 8] }
+def find_empty_lines(arr)
+  output = { 'empty_rows' => [], 'empty_columns' => [] }
+  arrays = [arr.dup, arr.dup.transpose]
+  arrays.each_with_index do |array, index|
+    array.each_with_index do |line, position|
+      next unless line.count('.') == line.length
+
+      key = index.zero? ? 'empty_rows' : 'empty_columns'
+      output[key] << position
+    end
+  end
+  output
+end
+
+# méthode pour calculer la distance entre 2 galaxies données
+# entrée => [0, 2], [9, 6]
+# sortie => 17 (si facteur 2), 49 (f = 10), 409 (f = 100)
+def calculate_distance(galaxy1, galaxy2, factor, empty_lines)
+  deltas = []
+  coordinates = [galaxy1.dup, galaxy2.dup].transpose.map(&:sort)
+  coordinates.each_with_index do |axe, index|
+    range = axe[0]..axe[1]
+    key = index.zero? ? 'empty_columns' : 'empty_rows'
+    n = empty_lines[key].select { |r| range.include?(r) }.length
+    deltas << ((n * factor) - n + axe[1] - axe[0])
+  end
+  deltas.sum
+end
+
+def answer2(galaxies, empty_lines, factor)
+  number_of_galaxies = galaxies.keys.length
+  pairs = (1..number_of_galaxies).to_a.combination(2).to_a
+  distances = []
+  pairs.each do  |pair|
+    distances << calculate_distance(galaxies[pair[0]], galaxies[pair[1]], factor, empty_lines)
+  end
+  distances.sum
+end
+
 # # # ANSWERS # # #
 
 # getting the data from the terminal
@@ -123,14 +112,15 @@ end
 
 puts '-----------'
 puts 'Réponse de la partie 1 :'
-expanded_universe = expand_universe(image_received)
+factor_part_one = 2
+expanded_universe = expand_universe(image_received, factor_part_one)
 all_galaxies = list_galaxies(expanded_universe)
 puts answer1(all_galaxies)
 puts '-----------'
 
-# puts 'Réponse de la partie 2 :'
-# puts '-----------'
-
-# méthode pour
-# entrée =>
-# sortie =>
+puts 'Réponse de la partie 2 :'
+factor_part_two = 1000000
+empty = find_empty_lines(image_received)
+all_galaxies = list_galaxies(image_received)
+puts answer2(all_galaxies, empty, factor_part_two)
+puts '-----------'
