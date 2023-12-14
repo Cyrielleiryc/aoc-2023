31868ed37eeed4d8db88e27816e4bce8802cc8fc
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-12-14 18:14:07 +0100

    day 8 part 2 OK

--- 08.rb
+++ 08.rb
@@ -20,66 +20,28 @@ end
 
 # # # PART TWO # # #
 
-test1_instructions = ['R', 'L']
-test1_nodes = {
-  'AAA' => ['BBB', 'CCC'],
-  'BBB' => ['DDD', 'EEE'],
-  'CCC' => ['ZZZ', 'GGG'],
-  'DDD' => ['DDD', 'DDD'],
-  'EEE' => ['EEE', 'EEE'],
-  'GGG' => ['GGG', 'GGG'],
-  'ZZZ' => ['ZZZ', 'ZZZ']
-}
-
-test2_instructions = ['L', 'L', 'R']
-test2_nodes = {
-  'AAA' => ['BBB', 'BBB'],
-  'BBB' => ['AAA', 'ZZZ'],
-  'ZZZ' => ['ZZZ', 'ZZZ']
-}
-
-test3_instructions = ['L', 'R']
-test3_nodes = {
-  '11A' => ['11B', 'XXX'],
-  '11B' => ['XXX', '11Z'],
-  '11Z' => ['11B', 'XXX'],
-  '22A' => ['22B', 'XXX'],
-  '22B' => ['22C', '22C'],
-  '22C' => ['22Z', '22Z'],
-  '22Z' => ['22B', '22B'],
-  'XXX' => ['XXX', 'XXX']
-}
-
 # méthode pour trouver les positions de départ
 def find_starts(arr)
   arr.select { |pos| pos[-1] == 'A' }
 end
 
-# méthode pour trouver la position suivante pour chaque trajet
-# entrée => [direction, positions, nodes]
-# sortie => [nouvelles positions]
-def next_positions(direction, positions, nodes)
-  key = direction == 'L' ? 0 : 1
-  positions.map { |pos| nodes[pos][key] }
-end
-
-# méthode pour dire si on est arrivé
-def reached_end(arr)
-  arr.length == arr.map { |p| p[-1] }.count('Z')
-end
-
 def answer2(instructions, nodes)
   positions = find_starts(nodes.keys)
-  count = 0
-  while !reached_end(positions)
-    instructions.each do |direction|
-      positions = next_positions(direction, positions, nodes)
-      count += 1
+  numbers = []
+  positions.each do |position|
+    pos = position
+    count = 0
+    until pos[-1] == 'Z'
+      instructions.each do |direction|
+        pos = next_element(direction, nodes[pos])
+        count += 1
+        break if pos[-1] == 'Z'
+      end
     end
+    numbers << count
   end
-  count
+  numbers.reduce(1, :lcm)
 end
-# puts answer2(test3_instructions, test3_nodes)
 
 # # # ANSWERS # # #
 
@@ -97,14 +59,10 @@ while input.downcase != 'fin'
 end
 
 puts '-----------'
-# puts 'Réponse de la partie 1 :'
-# puts answer1(instructions, nodes)
-# puts '-----------'
+puts 'Réponse de la partie 1 :'
+puts answer1(instructions, nodes)
+puts '-----------'
 
 puts 'Réponse de la partie 2 :'
 puts answer2(instructions, nodes)
 puts '-----------'
-
-# méthode pour
-# entrée =>
-# sortie =>
